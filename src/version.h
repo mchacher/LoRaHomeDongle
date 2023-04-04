69c99f65ccb6e4e3bf4e10badca71c904e8844f6
mchacher <marc.chachereau@gmail.com>
2023-04-04 20:14:36 +0200

    0.8.2 adding comments, remove all debug traces, and fixing a but with network id

---
+++ src/version.h
@@ -0,0 +1,19 @@
+/**
+ * @file version.h
+ * @author mchacher
+ * @brief 
+ * @version 0.8.2
+ * 0.8.2 adding comments, remove all debug traces, and fixing a but with network id
+ * @date 2023-04-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 8
+#define VERSION_PATCH 2
+
+#endif
