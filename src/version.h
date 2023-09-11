f4b7c11a55741206782284f2215a3420e40ae14a
mchacher <marc.chachereau@gmail.com>
2023-09-11 13:11:06 +0200

    10ms useless delay on task_uart_rx removed

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
  * @version 0.8.3
  * 0.8.2 01/04/2023 adding comments, remove all debug traces, and fixing a but with network id
  * 0.8.3 08/04/2023 feed watchdog when receiving lora messages - watchdog timer is set to 60s
- * 0.8.4 08/04/2023 remove useless delay in task_uart_rx to speed up transmission
+ * 0.8.4 11/09/2023 remove useless delay in task_uart_rx to speed up transmission
  * @date 2023-04-04
  * 
  * @copyright Copyright (c) 2023
